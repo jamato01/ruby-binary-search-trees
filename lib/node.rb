f4e215f74aefcaeea791aa7c0d1fd10d3820e985
jamato01 <jarrett.m.torres@gmail.com>
2025-10-27 16:32:10 -0400

    Refactor root variable to data

--- lib/node.rb
+++ lib/node.rb
@@ -1,15 +1,15 @@
 # This class creates a node, with left and right children data
 class Node
   include Comparable
-  attr_accessor :root, :left_node, :right_node
+  attr_accessor :data, :left_node, :right_node
 
-  def initialize(root, left_node = nil, right_node = nil)
-    @root = root
+  def initialize(data, left_node = nil, right_node = nil)
+    @data = data
     @left_node = left_node
     @right_node = right_node
   end
 
   def <=>(other)
-    root <=> other.root
+    data <=> other.data
   end
 end
