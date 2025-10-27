912585a7485a2871cc4a6741943ef778c8d22e7a
jamato01 <jarrett.m.torres@gmail.com>
2025-10-27 13:01:32 -0400

    Define methods for Node class
    Include comparable module to have <=> operator compare node root data
    Initialize with nil value defaults for left and right nodes

--- lib/node.rb
+++ lib/node.rb
@@ -1,4 +1,15 @@
 # This class creates a node, with left and right children data
 class Node
-  
+  include Comparable
+  attr_accessor :root, :left_node, :right_node
+
+  def initialize(root, left_node = nil, right_node = nil)
+    @root = root
+    @left_node = left_node
+    @right_node = right_node
+  end
+
+  def <=>(other)
+    root <=> other.root
+  end
 end
